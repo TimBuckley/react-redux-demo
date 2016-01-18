629fc46c5000d360dc1d5ca0a15823f984c5a4a6
Tim Buckley <timothypbuckley@gmail.com>
2016-01-17 22:00:51 -0500

    Start out with boilerplate from tutorial (to delete in next commit)

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Component } from 'react'
+
+export default React.createClass({
+  render: function() {
+    return(
+      <div>React simple starter</div>
+    )
+  }
+})
+
