f68df5276ad9c6a5d7e4ad72bebd4763f284a5aa
Tim Buckley <timothypbuckley@gmail.com>
2016-01-18 01:46:31 -0500

    Add new components to index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = React.createClass({
-  render: function() {
-    return(
-      <div>Hello World</div>
+import YTSearch from 'youtube-api-search'
+
+import SearchBar from './components/search-bar'
+import VideoList from './components/video-list'
+import VideoDetail from './components/video-detail'
+
+// const API_KEY = process.env.YOUTUBE_API_KEY
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+      term: ''
+    }
+
+    YTSearch(
+      {key: API_KEY, term: 'surfboards'},
+      (videos) => {
+        this.setState({
+          videos:videos,
+          selectedVideo: videos[0]
+        })
+      }
     )
   }
-})
+
+  render() {
+    return(
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={(selectedVideo) => this.setState({ selectedVideo })}
+          videos={this.state.videos}/>
+      </div>
+    )
+  }
+}
+
 
 ReactDOM.render(<App />, document.querySelector('.container'))
